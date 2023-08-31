b1bdb224a178ecd1c3efeb5d307805e97fa904d7
Ahmed <rifkiahmed.91@gmail.com>
2023-08-31 02:11:42 +0000

    Add leaf term definition

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - function that checks if a node is a leaf
+ * binary_tree_is_leaf - function that checks if a node is a leaf,
+ * node is a leaf if it doesn't have any child node.
  * @node: pointer to the node to check
  *
  * Return: 1 if node is a leaf, otherwise 0
